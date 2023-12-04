2dc7213babe3445c649f2d8ac26474c457162d28
Liliana Garcia <lilianagarciac@outlook.com>
2023-12-03 21:05:00 -0500

    second commit

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,66 @@
+import { useContext, useState } from "react";
+import AuthContext from "../context/AuthContext";
+
+const SignIn = () => {
+  let { setStep } = useContext(AuthContext);
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+ 
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [passwordFocused, setPasswordFocused] = useState(false);
+
+  const handlerSubmit = (e) => {
+    
+    e.preventDefault();
+    console.log("Sending data to backend...");
+    console.log(`El usuario digitó: ${email} y ${password}`);
+  };
+
+  return (
+    <main>
+      <h3>CLUSTER SEGUROS</h3>
+      <div className="card">
+        <form onSubmit={handlerSubmit} autoComplete="off">
+          <fieldset>
+            <label htmlFor="email">email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              autoFocus
+              required
+              onBlur={() => setEmailFocused(true)}
+              data-focused={emailFocused}
+            />
+            <p className="error">Valid email required</p>
+          </fieldset>
+          <fieldset>
+            <label htmlFor="password">password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+              pattern="(?=.*[\W])(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              required
+              onBlur={() => setPasswordFocused(true)}
+              data-focused={passwordFocused}
+            />
+            <p className="error">Valid password required</p>
+          </fieldset>
+          <button type="submit">login</button>
+          <div className="forgot" onClick={() => setStep("forgot")}>
+            Forgot password?
+          </div>
+        </form>
+      </div>
+      <p>
+        Don&apos;t have an account?{" "}
+        <span onClick={() => setStep("signup")}>Sign up</span>
+      </p>
+    </main>
+  );
+};
+
+export default SignIn;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,107 @@
+import { useContext } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import AuthContext from "../context/AuthContext";
+
+import { signUpSchema } from "../schemas/signUpSchema";
+
+const initialValues = {
+  fullName: "",
+  email: "",
+  password: "",
+  confirm: "",
+  framework: "",
+  terms: false,
+};
+
+const SignUp = () => {
+  let { setStep } = useContext(AuthContext);
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <main>
+      <h3> Bienvenido!</h3>
+      <div className="card">
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={signUpSchema}
+        >
+          <Form autoComplete="off">
+            <fieldset>
+              <label htmlFor="fullName">Full name</label>
+              <Field name="fullName" id="fullName" type="text" autoFocus />
+              <ErrorMessage
+                name="fullName"
+                component="p"
+                className="error-message"
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="email">email</label>
+              <Field name="email" id="email" type="email" />
+              <ErrorMessage
+                name="email"
+                component="p"
+                className="error-message"
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="password">password</label>
+              <Field name="password" id="password" type="password" />
+              <ErrorMessage
+                name="password"
+                component="p"
+                className="error-message"
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="confirm">Confirm password</label>
+              <Field name="confirm" id="confirm" type="password" />
+              <ErrorMessage
+                name="confirm"
+                component="p"
+                className="error-message"
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="framework">Favorite framework</label>
+              <Field name="framework" id="framework" as="select">
+                <option value="">Select your framework</option>
+                <option value="react">React</option>
+                <option value="vue">Vue</option>
+                <option value="angular">Angular</option>
+              </Field>
+              <ErrorMessage
+                name="framework"
+                component="p"
+                className="error-message"
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="terms">
+                <Field name="terms" id="terms" type="checkbox" /> Accept terms
+                and conditions
+              </label>
+              <ErrorMessage
+                name="terms"
+                component="p"
+                className="error-message error-message-terms"
+              />
+            </fieldset>
+            <button type="submit">Register</button>
+          </Form>
+        </Formik>
+      </div>
+      <p>
+        Already have an account?{" "}
+        <span onClick={() => setStep("signin")}>Sign in</span>
+      </p>
+    </main>
+  );
+};
+
+export default SignUp;
+
